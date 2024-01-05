fdc486379835f529fa02d626aca30a8b59166a24
rosendyakov <r.dyakov1314@gmail.com>
2024-01-05 14:46:16 +0200

    feat: create SearchForm component

---
+++ src/components/searchForm/SearchForm.tsx
@@ -0,0 +1,75 @@
+import { useRef, useState } from "react";
+
+import { Box, Grid, Paper, Typography } from "@mui/material";
+import styles from "./searchForm.module.scss";
+import { SearchInput } from "./input/Input";
+import { RadioButtons } from "./radioButtons/RadioButtons";
+import { SubmitButton } from "./submitButton/SubmitButton";
+import { CategoryEnum, categories } from "../../utils/constants/constants";
+import { useResults } from "../../queries/results";
+import { useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { getResults } from "../../services/getResults";
+
+export const SearchForm = () => {
+  // useRef instead of useState because it's unnecessary to re-render the component when the input value changes
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [radioValue, setRadioValue] = useState(CategoryEnum.All);
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRadioValue(event.target.value as CategoryEnum);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await queryClient.fetchQuery({
+      queryKey: ["results"],
+      queryFn: () => getResults(radioValue, inputRef.current?.value),
+    });
+    const url = new URLSearchParams({
+      category: radioValue,
+      query: inputRef.current?.value as string,
+    });
+    navigate("/search-results?" + url.toString());
+  };
+
+  return (
+    <div className={styles.searchForm}>
+      <Paper elevation={2}>
+        <Grid container flexDirection={"row"} p={2}>
+          <Grid item>
+            <Box
+              sx={{
+                marginTop: 2,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                Search
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+              >
+                <SearchInput inputRef={inputRef} />
+                <RadioButtons
+                  categories={categories}
+                  radioValue={radioValue}
+                  onChange={handleChange}
+                />
+                <SubmitButton />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
