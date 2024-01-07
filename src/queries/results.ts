d412964d5fd9395b57b97ba9a89211785bc55dca
rosendyakov <r.dyakov1314@gmail.com>
2024-01-07 13:10:50 +0200

    refactor: add staleTime parameter to query to prevent unnecessary refetching

--- src/queries/results.ts
+++ src/queries/results.ts
@@ -2,5 +2,9 @@ import { useQuery } from "@tanstack/react-query"
 import { getResults } from "../services/getResults";
 
 export const useResults = (category: string, searchQuery: string) => {  
-  return useQuery({queryKey: ["results", category, searchQuery], queryFn: () => getResults(category, searchQuery)});
+  return useQuery({
+  queryKey: ["results", category, searchQuery], 
+   // staleTIme is needed because react router unmounts and mounts the component and react query makes a request every time the component is mounted, which we are trying to avoid here.
+  queryFn: () => getResults(category, searchQuery), staleTime: 1000 * 60 * 5});
+
 }
