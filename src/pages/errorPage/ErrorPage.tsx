6f77460d86bf16ebdca53df6471e8e20c0268e16
rosendyakov <r.dyakov1314@gmail.com>
2024-01-07 19:10:23 +0200

    refactor: uodate Error page design

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,12 +1,33 @@
+import { Button, Grid, Typography } from "@mui/material";
+import { PageContainer } from "../../components/pageContainer/PageContainer";
+import { Link } from "react-router-dom";
+
 export const ErrorPage = () => {
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i></i>
-      </p>
-      <button>Back</button>
-    </div>
+    <PageContainer>
+      <Grid
+        container
+        p={3}
+        flexDirection={"column"}
+        alignItems={"center"}
+        gap={3}
+      >
+        <Grid item>
+          <Typography component="h1" variant="h4" align="center">
+            Oops!
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">
+            Sorry, an unexpected error has occurred.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Link to={"/"}>
+            <Button variant="contained">Back</Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </PageContainer>
   );
 };
