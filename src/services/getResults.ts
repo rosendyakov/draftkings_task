e0dc0816080f7f9f42fda6ad3e2e486c6d935c0c
rosendyakov <r.dyakov1314@gmail.com>
2024-01-07 13:11:20 +0200

    feat: create and assign types to the data returned from the API

--- src/services/getResults.ts
+++ src/services/getResults.ts
@@ -1,23 +1,33 @@
 import { BASE_URL, CategoryEnum, ENDPOINTS } from "../utils/constants/constants"
+import { AllApiResponses, CategoryApiResponse } from "../utils/types/types";
 
-const getData = async (category: string, searchQuery: string) => {
+
+
+const getData = async (category: string, searchQuery: string): Promise<CategoryApiResponse> => {
   const url = new URL(`${BASE_URL}${category.toLowerCase()}`);
-  if(searchQuery) {
-    url.searchParams.append('search', searchQuery);    
+  if (searchQuery) {
+    url.searchParams.append('search', searchQuery);
   }
   const response = await fetch(url.toString());
   return response.json();
 };
 
-const getResults = async (category: string, searchQuery: string) => {
+const getAllData = async (searchQuery: string): Promise<AllApiResponses> => {
+  const promises = ENDPOINTS.map(endpoint => getData(endpoint, searchQuery));
+  const response = await Promise.all(promises);
+  return response;
+};
+
+const getResults = async (category: string, searchQuery: string): Promise<AllApiResponses | CategoryApiResponse> => {
   if (category.toLowerCase() === CategoryEnum.All) {
-    const promises = ENDPOINTS.map(endpoint => getData(endpoint, searchQuery));
-    return await Promise.all(promises);
+    const response = await getAllData(searchQuery);
+    return response;
   } else {
-    return await getData(category, searchQuery);
+    const response = await getData(category, searchQuery);
+    return response;
   }
-}
+};
 
 export {
   getResults
-}
+};
--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -1,5 +1,125 @@
-export type Category = {
+
+type Category = {
   id: number;
   label: string;
   value: string;
 };
+
+type Person = {
+  birth_year: string;
+  eye_color: string;
+  films: string[];
+  gender: string;
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  name: string;
+  skin_color: string;
+  created: string;
+  edited: string;
+  species: string[];
+  starships: string[];
+  url: string;
+  vehicles: string[];
+}
+
+type Planet = {
+  climate: string;
+  created: string;
+  diameter: string;
+  edited: string;
+  films: string[];
+  gravity: string;
+  name: string;
+  orbital_period: string;
+  population: string;
+  residents: string[];
+  rotation_period: string;
+  surface_water: string;
+  terrain: string;
+  url: string;
+};
+
+type Vehicle = {
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: string;
+  crew: string;
+  edited: string;
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  pilots: never[]; // no data on the API
+  films: string[];
+  url: string;
+  vehicle_class: string;
+};
+
+type Starship = {
+  MGLT: string;
+  cargo_capacity: string;
+  consumables: string;
+  cost_in_credits: string;
+  created: string;
+  crew: string;
+  edited: string;
+  hyperdrive_rating: string;
+  length: string;
+  manufacturer: string;
+  max_atmosphering_speed: string;
+  model: string;
+  name: string;
+  passengers: string;
+  films: string[];
+  pilots: never[]; // no data on the API
+  starship_class: string;
+  url: string;
+};
+
+interface ApiResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+}
+
+interface PeopleApiResponse extends ApiResponse {
+  results: Person[];
+}
+
+interface PlanetsApiResponse extends ApiResponse {
+  results: Planet[];
+}
+
+interface VehiclesApiResponse extends ApiResponse {
+  results: Vehicle[];
+}
+
+interface StarshipsApiResponse extends ApiResponse {
+  results: Starship[];
+}
+
+
+type CategoryApiResponse = PeopleApiResponse | PlanetsApiResponse | VehiclesApiResponse | StarshipsApiResponse;
+
+type AllApiResponses = PeopleApiResponse | PlanetsApiResponse | VehiclesApiResponse | StarshipsApiResponse | CategoryApiResponse[];
+
+
+export type {
+  Category,
+  ApiResponse,
+  Person,
+  Planet,
+  Vehicle,
+  Starship,
+  PeopleApiResponse,
+  PlanetsApiResponse,
+  VehiclesApiResponse,
+  StarshipsApiResponse,
+  CategoryApiResponse,
+  AllApiResponses
+} 
