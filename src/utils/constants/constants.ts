0c5811591ea956f87cfc16101506ace28cacdcee
rosendyakov <r.dyakov1314@gmail.com>
2024-01-08 17:19:32 +0200

    feat: add columns as constants

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -22,4 +22,36 @@ const ENDPOINTS = ["people", "planets", "vehicles", "starships"]
 
 const QUERY_STALE_TIME = 1000 * 60 * 5 // 5 mins
 
-export { BASE_URL, ENDPOINTS, CATEGORIES, CATEGORY_ENUM, QUERY_STALE_TIME}
+const PERSON_COLUMNS = [
+  { label: 'Name', key: 'name' },
+  { label: 'Birth Year', key: 'birth_year' },
+  { label: 'Species', key: 'species' },
+  { label: 'Starships', key: 'starships' },
+  { label: 'Vehicles', key: 'vehicles' },
+];
+
+const PLANET_COLUMNS = [
+  { label: 'Name', key: 'name' },
+  { label: 'Climate', key: 'climate' },
+  { label: 'Diameter', key: 'diameter' },
+  { label: 'Gravity', key: 'gravity' },
+  { label: 'Residents', key: 'residents' },
+];
+
+const VEHICLE_COLUMNS = [
+  { label: 'Name', key: 'name' },
+  { label: 'Model', key: 'model' },
+  { label: 'Manufacturer', key: 'manufacturer' },
+  { label: 'Cost in Credits', key: 'cost_in_credits' },
+  { label: 'Length', key: 'length' },
+];
+
+const STARSHIP_COLUMNS = [
+  { label: 'Name', key: 'name' },
+  { label: 'Model', key: 'model' },
+  { label: 'Manufacturer', key: 'manufacturer' },
+  { label: 'Cost in Credits', key: 'cost_in_credits' },
+  { label: 'Length', key: 'length' },
+];
+
+export { BASE_URL, ENDPOINTS, CATEGORIES, CATEGORY_ENUM, QUERY_STALE_TIME, PERSON_COLUMNS, PLANET_COLUMNS, VEHICLE_COLUMNS, STARSHIP_COLUMNS}
