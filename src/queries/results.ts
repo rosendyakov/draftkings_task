e278b42f281daa176115b1d582a9a27fa252ed00
rosendyakov <r.dyakov1314@gmail.com>
2024-01-08 17:18:16 +0200

    feat: add new query hooks

--- src/queries/results.ts
+++ src/queries/results.ts
@@ -1,11 +1,35 @@
 import { useQuery } from "@tanstack/react-query"
-import { getResults } from "../services/getResults";
+import { getFilms, getResults, getSingleResult } from "../services/getResults";
 import { QUERY_STALE_TIME } from "../utils/constants/constants";
 
-export const useResults = (category: string, searchQuery: string) => {  
+const useResults = (category: string, searchQuery: string) => {  
   return useQuery({
   queryKey: ["results", category, searchQuery], 
-   // staleTIme is needed because react router unmounts and mounts the component and react query makes a request every time the component is mounted, which we are trying to avoid here.
+   // staleTIme is needed because react router unmounts and
+   // mounts the component and react query makes a request every 
+   // time the component is mounted, which we are trying to avoid here.
   queryFn: () => getResults(category, searchQuery), staleTime: QUERY_STALE_TIME});
 
+}
+
+const useFilms = (urls: string[]) => {
+  return useQuery({
+    queryKey: ["films", urls],
+    queryFn: () => getFilms(urls),
+    staleTime: QUERY_STALE_TIME
+  })
+}
+
+const useSingleResult = (category: string, id: string) => {
+  return useQuery({
+    queryKey: [category, id],
+    queryFn: () => getSingleResult(category, id),
+    staleTime: QUERY_STALE_TIME
+  })
+}
+
+export {
+  useResults,
+  useFilms,
+  useSingleResult
 }
