07cf3fb1fc3d586cf6da2adbacf9e8d697c3ac7f
rosendyakov <r.dyakov1314@gmail.com>
2024-01-09 14:39:14 +0200

    refactor: update the category components to handle the master-details pattern

--- src/components/resultsList/allCategoriesList/AllCategoriesList.tsx
+++ src/components/resultsList/allCategoriesList/AllCategoriesList.tsx
@@ -1,35 +1,63 @@
-import { sortedArrayDesc } from "../../../utils/utils";
-import { CategoryApiResponse } from "../../../utils/types/types";
-import { Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { getFilmIds, sortedArrayDesc } from "../../../utils/utils";
+import {
+  CategoryApiResponse,
+  Person,
+  Planet,
+  Starship,
+  Vehicle,
+} from "../../../utils/types/types";
+import { Button, Grid, Typography } from "@mui/material";
+import { useState } from "react";
+import { Details } from "../../details/Details";
 
 interface AllCategoriesListProps {
   data: CategoryApiResponse[];
 }
 
 export const AllCategoriesList = ({ data }: AllCategoriesListProps) => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item: Person | Planet | Starship | Vehicle) => {
+    setSelectedItem(item);
+  };
   return (
-    <div>
-      {sortedArrayDesc(data).map((category) => {
-        return (
-          <div key={category.count}>
-            <Typography variant="h6" m={1}>
-              Results: {category.count}
-            </Typography>
-            <ul>
-              {category.results.map((result) => {
-                return (
-                  <li>
-                    <Link to="" key={result.name}>
-                      {result.name}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        );
-      })}
-    </div>
+    <Grid container flexDirection={"row"}>
+      <Grid item>
+        {sortedArrayDesc(data).map((category) => {
+          return (
+            <Grid item key={category.count}>
+              <Typography variant="h6" m={1}>
+                Results: {category.count}
+              </Typography>
+              <ul>
+                {category.results.map((result) => {
+                  return (
+                    <li key={result.name}>
+                      <Button
+                        variant="text"
+                        sx={{
+                          textTransform: "capitalize",
+                          textDecoration: "underline",
+                        }}
+                        onClick={() => handleItemClick(result)}
+                      >{`${result.name} (appears in ${getFilmIds(
+                        result.films
+                      )})`}</Button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Grid item maxWidth={250} overflow={"hidden"}>
+        {selectedItem && (
+          <Grid container flexDirection={"column"}>
+            <Details item={selectedItem} />
+          </Grid>
+        )}
+      </Grid>
+    </Grid>
   );
 };
--- src/components/resultsList/categoryList/CategoryList.tsx
+++ src/components/resultsList/categoryList/CategoryList.tsx
@@ -1,29 +1,71 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { Person, Planet, Starship, Vehicle } from "../../../utils/types/types";
 import { getFilmIds } from "../../../utils/utils";
-import { Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
+import { Details } from "../../details/Details";
 
-type CategoryListProps = {
-  data: Person[] | Planet[] | Vehicle[] | Starship[];
+type PersonDetailsProps = {
+  data: Person[] | null;
 };
 
-export const CategoryList = ({ data }: CategoryListProps) => {
+type PlanetDetailsProps = {
+  data: Planet[] | null;
+};
+
+type VehicleDetailsProps = {
+  data: Vehicle[] | null;
+};
+
+type StarshipDetailsProps = {
+  data: Starship[] | null;
+};
+
+// TODO - finish up the master-details design , add pagination
+export const CategoryList = ({
+  data,
+}:
+  | PersonDetailsProps
+  | PlanetDetailsProps
+  | VehicleDetailsProps
+  | StarshipDetailsProps) => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (item: Person | Planet | Starship | Vehicle) => {
+    setSelectedItem(item);
+  };
+
   return (
-    <div>
-      <Typography variant="h6" m={1}>
-        Results: {data.length}
-      </Typography>
-      <ul>
-        {data.map((result) => {
-          return (
-            <li key={result.name}>
-              <Link to="">{`${result.name} (appears in ${getFilmIds(
-                result.films
-              )})`}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <Grid container flexDirection={"row"} gap={3}>
+      <Grid item>
+        <Typography variant="h6" m={1}>
+          Results: {data?.length}
+        </Typography>
+        <ul>
+          {data?.map((result) => {
+            return (
+              <li key={result.name}>
+                <Button
+                  variant="text"
+                  sx={{
+                    textTransform: "capitalize",
+                    textDecoration: "underline",
+                  }}
+                  onClick={() => handleItemClick(result)}
+                >{`${result.name} (appears in ${getFilmIds(
+                  result.films
+                )})`}</Button>
+              </li>
+            );
+          })}
+        </ul>
+      </Grid>
+      <Grid item maxWidth={250} overflow={"hidden"}>
+        {selectedItem && (
+          <Grid container flexDirection={"column"}>
+            <Details item={selectedItem} />
+          </Grid>
+        )}
+      </Grid>
+    </Grid>
   );
 };
